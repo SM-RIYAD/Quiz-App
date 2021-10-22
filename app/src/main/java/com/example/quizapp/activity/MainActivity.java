a0bd5036e9700259635ee12edba9f5cd90ae2f3a
SM-RIYAD <88622536+SM-RIYAD@users.noreply.github.com>
2021-10-22 16:28:45 +0600

    completed with out admin panel

--- app/src/main/java/com/example/quizapp/activity/MainActivity.java
+++ app/src/main/java/com/example/quizapp/activity/MainActivity.java
@@ -6,12 +6,16 @@ import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.ScrollingView;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.quizapp.R;
 import com.example.quizapp.classes.QuizDS;
@@ -22,21 +26,25 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 private View progress_bar_view;
-private TextView question_count_text,text_option1,text_option2,text_option3,text_option4,temp,text_question;
+private TextView question_count_text,text_option1,text_option2,text_option3,text_option4,
+        temp,text_question,text_score_percentage,text_counter,text_message;
 private ScrollView scroll_content;
-private  String quiz_id;
+private ProgressBar progress_score_bar;
+private  String quiz_id,selected_ans="";
 private ArrayList<QuizDS>arrayList;
-private int index=0;
+private int index=0,score_progress = 0;
+private long total_questions;
+    private ArrayList<String> user_answers,correct_answers;
+private CardView card_answer,card_questions,card_result;
+private Button button_quit,button_play;
+    private Widgetcontroller widgetcontroller ;
 
-private CardView card_answer,card_questions;
-
-    public MainActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +53,26 @@ private CardView card_answer,card_questions;
         text_question= findViewById(R.id.text_question);
             quiz_id= getIntent().getStringExtra("quiz_id");
         progress_bar_view= findViewById(R.id.progress_bar_view);
+        progress_score_bar=findViewById(R.id.  progress_score_bar);
         text_option1=findViewById(R.id.text_option1);
+
+        text_message=findViewById(R.id. text_message);
         text_option2=findViewById(R.id.text_option2);
+        text_counter=findViewById(R.id.text_counter);
         text_option3=findViewById(R.id.text_option3);
         text_option4=findViewById(R.id.text_option4);
+        button_quit=findViewById(R.id.button_quit);
+        button_play=findViewById(R.id.button_play);
         card_answer=findViewById(R.id.card_answer);
+        text_score_percentage= findViewById(R.id. text_score_percentage);
         scroll_content=findViewById(R.id.scroll_content);
         card_questions=findViewById(R.id.card_questions);
+        card_result=findViewById(R.id.card_result);
         question_count_text=findViewById(R.id.question_count_text);
-new Widgetcontroller(progress_bar_view,question_count_text).update_progress();
+        user_answers = new ArrayList<>();
+        correct_answers = new ArrayList<>();
+        text_counter.setText("1");
+
 getQuizData();
 arrayList = new ArrayList<>();
 card_answer.setOnClickListener(this);
@@ -61,6 +80,8 @@ card_answer.setOnClickListener(this);
         text_option2.setOnClickListener(this);
         text_option3.setOnClickListener(this);
         text_option4.setOnClickListener(this);
+        button_play.setOnClickListener(this);
+        button_quit.setOnClickListener(this);
 
 temp=text_option1;
     }
@@ -70,6 +91,9 @@ temp=text_option1;
 databaseReference.child(quiz_id).addValueEventListener(new ValueEventListener() {
     @Override
     public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+        total_questions= snapshot.getChildrenCount();
+        question_count_text.setText(MessageFormat.format("0/{0}",total_questions));
         for(DataSnapshot dataSnapshot:snapshot.getChildren()){
           ///  text_question.setText(Objects.requireNonNull(dataSnapshot.child("que").getValue()).toString());
           ///  text_option1.setText(Objects.requireNonNull(dataSnapshot.child("_1").getValue()).toString());
@@ -81,6 +105,7 @@ arrayList.add(new QuizDS(Objects.requireNonNull(dataSnapshot.child("que").getVal
 
 
         }
+        widgetcontroller = new Widgetcontroller(progress_bar_view,question_count_text, total_questions);
         assignDataToUI();
     }
 
@@ -108,41 +133,49 @@ arrayList.add(new QuizDS(Objects.requireNonNull(dataSnapshot.child("que").getVal
     public void onClick(View v) {
 
         if(v==text_option1)
-        {             temp.setBackgroundTintList(null);
-            temp=text_option1;
-            text_option1.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.purple_500));
+        {   handleClicks(text_option1);
 
 
 
 
         }
+
+        if(v== button_play){
+
+            resetEverything();
+        }
+        if (v == button_quit) {
+            exitApp();
+        }
         if(v==text_option2){
-            temp.setBackgroundTintList(null);
-              temp=text_option2;
-            text_option2.setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.purple_500));
+            handleClicks(text_option2);
 
 
 
 
         }
         if(v==text_option3){
-            temp.setBackgroundTintList(null);
-            temp=text_option3;
-            text_option3.setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.purple_500));
+            handleClicks(text_option3);
 
 
 
         }
         if(v==text_option4){
-            temp.setBackgroundTintList(null);
-            temp=text_option4;
-            text_option4.setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.purple_500));
+//            temp.setBackgroundTintList(null);
+//            temp=text_option4;
+//            text_option4.setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.purple_500));
 
-
+            handleClicks(text_option4);
 
 
         }
         if(v==card_answer){
+            if(selected_ans.isEmpty()){
+
+                Toast.makeText(getApplicationContext(), "Select an option first!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            widgetcontroller.update_progress();
             card_questions.setLayerType(View.LAYER_TYPE_HARDWARE,null);
             card_questions.animate().scaleX(.3f).scaleY(.2f).setDuration(500).setInterpolator(new OvershootInterpolator());
             scroll_content.animate().alpha(0).setDuration(500);
@@ -151,7 +184,31 @@ arrayList.add(new QuizDS(Objects.requireNonNull(dataSnapshot.child("que").getVal
                 public void run() {
                     card_questions.animate().scaleX(1).scaleY(1).setDuration(500).setInterpolator(new OvershootInterpolator());
                     scroll_content.animate().alpha(1).setDuration(500);
+                    user_answers.add(selected_ans);
+                    if(index+1==total_questions){
+                        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Answers");
+databaseReference.child(quiz_id).addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(@NonNull DataSnapshot snapshot) {
+        for (DataSnapshot dataSnapshot:snapshot.getChildren()) {
+            correct_answers.add(Objects.requireNonNull(dataSnapshot.getValue()).toString());
+        }
+        makeScoreUIVisible();
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
+    }
+});
+
+
+                        return;
+                    }
+
+
                     index++;
+                    text_counter.setText(String.valueOf(index + 1));
                     assignDataToUI();
                 }
             },500);
@@ -161,5 +218,77 @@ arrayList.add(new QuizDS(Objects.requireNonNull(dataSnapshot.child("que").getVal
         }
 
 
+    }
+
+    private void exitApp() {
+        finishAffinity();
+
+    }
+
+    private void resetEverything() {
+
+        index = 0;
+        assignDataToUI();
+        progress_bar_view.setScaleX(0);
+       /// card_score.setVisibility(View.GONE);
+        question_count_text.setText(MessageFormat.format("0/{0}", total_questions));
+        card_result.setVisibility(View.GONE);
+        card_questions.setVisibility(View.VISIBLE);
+        card_answer.setVisibility(View.VISIBLE);
+        user_answers.clear();
+        correct_answers.clear();
+        arrayList.clear();
+        getQuizData();
+        score_progress = 0;
+        total_questions = 0;
+        progress_bar_view.setScaleX(0);
+        text_counter.setText(String.valueOf(index + 1));
+    }
+
+    private void makeScoreUIVisible() {
+        for (int i=0; i<correct_answers.size(); i++) {
+
+            Toast.makeText(getApplicationContext(),"INSIDE!", Toast.LENGTH_SHORT).show();
+
+            //this portion is not working...for some reason. fix it for BDT 100.
+            if (user_answers.get(i).equals(correct_answers.get(i))) {
+
+                score_progress += (100 / correct_answers.size());
+
+
+            }
+        }
+        displayMessage();
+
+
+        text_score_percentage.setText(MessageFormat.format("{0}%", score_progress));
+        card_result.setVisibility(View.VISIBLE);
+        card_questions.setVisibility(View.GONE);
+        card_answer.setVisibility(View.GONE);
+
+        ObjectAnimator progressAnimator;
+      progressAnimator = ObjectAnimator.ofInt(progress_score_bar,"progress", 0,score_progress);
+        progressAnimator.setDuration(1000);
+        progressAnimator.setInterpolator(new OvershootInterpolator());
+        progressAnimator.start();
+    }
+
+    private void displayMessage() {
+        if(score_progress>65){
+            text_message.setText("AWSOME!!");
+
+        }
+        if(score_progress<50){
+            text_message.setText("You can do better!!");
+
+        }
+    }
+
+
+    private void handleClicks(TextView tv) {
+        selected_ans=tv.getText().toString().trim();
+        temp.setBackgroundTintList(null);
+        temp = tv;
+        tv.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.purple_500));
     }
 }
--- app/src/main/java/com/example/quizapp/classes/Widgetcontroller.java
+++ app/src/main/java/com/example/quizapp/classes/Widgetcontroller.java
@@ -11,29 +11,35 @@ public class Widgetcontroller {
 
     public View progress_bar_view;
     private TextView question_count_text;
-    private  double scale =0.04;
+    private  double scale ;
+   private long total_questions=0;
 public int count =1;
-    public Widgetcontroller(View progress_bar_view, TextView question_count_text) {
-        this.progress_bar_view = progress_bar_view;
-        this.question_count_text= question_count_text;
 
+    public Widgetcontroller(View progress_bar_view, TextView question_count_text, long total_questions) {
+        this.progress_bar_view = progress_bar_view;
+        this.question_count_text = question_count_text;
+        this.total_questions = total_questions;
+        scale=1/(float)total_questions;
     }
 
     public void update_progress() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-          question_count_text.setText(MessageFormat.format("{0}/25", count));
-          count++;
-                progress_bar_view.animate().scaleX((float)scale).setDuration(700).setInterpolator(new OvershootInterpolator());
-                ///   progress_bar_view.setScaleX((float) scale);
-                if(scale>1){return;}
-                scale+=0.04;
 
-                update_progress();
 
-            }
-        },1000);
+        question_count_text.setText(MessageFormat.format("{0}/{1}", count,total_questions));
+        count++;
+        progress_bar_view.animate().scaleX((float)scale).setDuration(700).setInterpolator(new OvershootInterpolator());
+        ///   progress_bar_view.setScaleX((float) scale);
+        if(scale>1){return;}
+        scale+=1/(float)total_questions;
+
+        ///update_progress();
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//
+//            }
+//        },1000);
 
 
 
